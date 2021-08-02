84c60b1c01053125c51ee4ccd5871cd53a57c521
Palladium02 <seanmeyer544@gmail.com>
2021-08-02 15:02:34 +0200

    added authGuard logic

---
+++ src/utils/authGuard.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from 'express';
+import authentication from '../services/auth/Authentication';
+
+interface authGuardOptions {
+  req: Request,
+  res: Response,
+  accepted: (req: Request, res: Response) => {},
+  declined: (req: Request, res: Response) => {};
+}
+
+const authGuard = ({ req, res, accepted, declined }: authGuardOptions) => {
+  let sessionCookie = req.signedCookies['session'];
+  let sessionToken = req.body?.sessionToken;
+
+  if(!req.body) req['body'] = {};
+
+  if(!sessionCookie) {
+    if (!authentication.isValidId(sessionToken)) declined(req, res);
+  } else if(sessionCookie) {
+    if (!authentication.isValidId(sessionCookie)) declined(req, res);
+  } else {
+    if (!authentication.isValidId(sessionToken)) declined(req, res);
+  }
+
+  if(sessionCookie) {
+    req.body.id = sessionCookie;
+  } else if(sessionToken) {
+    req.body.id = sessionToken;
+  }
+
+  accepted(req, res);
+};
+
+export default authGuard;
